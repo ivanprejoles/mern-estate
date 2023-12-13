28370ad9debbf1a489bb3c211c2ceb072121f9d6
ivanprejoles <ivanpatrickprejoles@gmail.com>
2023-12-13 08:25:28 +0800

    update user API comleted

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,4 +1,4 @@
-import express from "express";
+ import express from "express";
 import { test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,10 +21,29 @@ const userSlice = createSlice({
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
+        },
+        updateUserstart : (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess : (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateUserFailure : (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
         }
     }
 })
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+export const {
+    signInStart, 
+    signInSuccess, 
+    signInFailure, 
+    updateUserstart, 
+    updateUserSuccess, 
+    updateUserFailure
+} = userSlice.actions;
 
 export default userSlice.reducer;
